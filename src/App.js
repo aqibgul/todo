32059aa74b37dfba6b541e5d8dd3d1d37ed31dbe
aqib gul <aqibgul880@gmail.com>
2025-05-09 10:02:25 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MdDelete } from "react-icons/md";
+import { IoIosAddCircle } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [task, setTask] = useState([]); // Array to store tasks
+
+  const handleOnchange = (text) => {
+    setInputText(text);
+  };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    if (!inputText) return;
+    if (task.includes(inputText)) {
+      alert("Item already exists");
+      setInputText("");
+      return;
+    }
+
+    setTask((prevTask) => [...prevTask, inputText]); // Add new task to the array
+    console.log(task);
+    setInputText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+        <h1>My Todo List</h1>
+        <form onSubmit={handleSubmitForm}>
+          <input
+            type="text"
+            placeholder="Add a new task"
+            value={inputText}
+            onChange={(event) => handleOnchange(event.target.value)}
+          />
+          <button>
+            <IoIosAddCircle className="add-btn" />
+          </button>
+        </form>
+
+        <ul className="">
+          {task.map((item, index) => (
+            <div className="">
+              <li className="list-items" key={index}>
+                <span>{item}</span>
+                <button
+                  className="dlt-btn"
+                  onClick={() => {
+                    setTask((prevTask) =>
+                      prevTask.filter((_, items) => items !== index)
+                    );
+                    alert("Confirm Item deleted");
+                  }}
+                >
+                  <MdDelete />
+                </button>
+                <button>
+                  <FaRegEdit
+                    onClick={() => {
+                      const updatedTask = prompt("Update your task", item);
+                      console.log(item);
+                      if (updatedTask) {
+                        setTask((prevTask) =>
+                          prevTask.map((task, i) =>
+                            i === index ? updatedTask : task
+                          )
+                        );
+                      }
+                    }}
+                  />
+                </button>
+
+                <input
+                  type="checkbox"
+                  onChange={(event) => {
+                    if (event.target.checked) {
+                      event.target.parentElement.style.textDecoration =
+                        "line-through";
+                    } else {
+                      event.target.parentElement.style.textDecoration = "none";
+                    }
+                  }}
+                />
+              </li>
+            </div>
+          ))}
+        </ul>
+        <button>
+          <span
+            onClick={() => {
+              setTask([]);
+            }}
+          >
+            Clear All
+          </span>
+        </button>
+      </div>
+    </>
   );
 }
 
